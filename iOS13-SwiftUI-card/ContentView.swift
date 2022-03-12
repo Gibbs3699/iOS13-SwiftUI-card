6711f9001b998237803225d4bf37174e89fe7e87
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-03-12 21:14:05 +0700

    Initial Commit

---
+++ iOS13-SwiftUI-card/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOS13-SwiftUI-card
+//
+//  Created by admin on 12/3/2565 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOS13-SwiftUI-card/iOS13_SwiftUI_cardApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS13_SwiftUI_cardApp.swift
+//  iOS13-SwiftUI-card
+//
+//  Created by admin on 12/3/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct iOS13_SwiftUI_cardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
